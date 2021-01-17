f801be4e8bc477a3d55203db3f85503c2d9867f2
ischeinkman <scheinkman.ilan@gmail.com>
2021-01-16 23:48:57 -0800

    Collected plugin group, config, and entry cache into singe "state" struct.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod model;
 use model::{Config, EntryPlugin, ListEntry};
 
 use std::borrow::Cow;
-use std::collections::BTreeMap;
+use std::collections::HashMap;
 
 fn main() {
     let config = Config {
@@ -21,46 +21,18 @@ fn main() {
 
     let xdgp = FreedesktopPlugin::new();
     let ptp = RawPathPlugin::new();
-
-    let mut plugins = PluginsGroup::new();
-    plugins.push(xdgp);
-    plugins.push(ptp);
-    plugins.start(&config);
-
-    let mut entries: BTreeMap<String, ListEntry> = BTreeMap::new();
-    let mut idx = 0;
-    for mut ent in plugins {
-        if let Some(cur_ent) = entries.get_mut(ent.exec_name().unwrap()) {
-            let should_replace = (ent.exec_command.len() == 1 && cur_ent.exec_command.len() != 1)
-                || (ent.display_name.is_some() && cur_ent.display_name.is_none());
-            if should_replace {
-                for child in cur_ent.children.drain(..) {
-                    ent.children.push(child);
-                }
-                std::mem::swap(cur_ent, &mut ent);
-            }
-            let should_push = ent.exec_command != cur_ent.exec_command
-                && (ent.exec_command.len() != 1 || cur_ent.exec_command.len() != 1);
-            if should_push {
-                cur_ent.children.push(ent);
-                idx += 1;
-            }
-        } else {
-            entries.insert(ent.exec_name().unwrap().to_owned(), ent);
-            idx += 1;
-        }
-        if idx == config.list_size {
-            println!("====== =================== ======");
-            println!("====== STARTING LIST PRINT ======");
-            println!("====== =================== ======");
-            for ent in entries.values() {
-                print_recursive(ent, 0);
-            }
-            println!("====== =================== ======");
-            println!("====== StOPPING LIST PRINT ======");
-            println!("====== =================== ======");
-            idx = 0;
-        }
+    let mut state = State::new(config);
+    state.push_plugin(xdgp);
+    state.push_plugin(ptp);
+    state.start();
+    let argv: Vec<_> = std::env::args().collect();
+    let res = if argv.len() > 1 {
+        state.search_loaded(argv.last().unwrap())
+    } else {
+        state.all_entries()
+    };
+    for itm in res {
+        print_recursive(&itm, 0);
     }
 }
 
@@ -80,36 +52,92 @@ fn print_recursive(entry: &ListEntry, level: usize) {
     println!("\n");
 }
 
-#[derive(Default)]
-pub struct PluginsGroup {
+pub struct State {
+    config: Config,
+    entries: HashMap<String, ListEntry>,
     plugins: Vec<Box<dyn EntryPlugin>>,
 }
 
-impl PluginsGroup {
-    pub fn new() -> Self {
+fn matches_search(key: &str, ent: &ListEntry) -> bool {
+    ent.name().to_lowercase().contains(key)
+        || ent
+            .search_terms
+            .iter()
+            .any(|term| term.to_lowercase().contains(key))
+}
+
+impl State {
+    pub fn new(config: Config) -> Self {
         Self {
+            config,
+            entries: HashMap::new(),
             plugins: Vec::new(),
         }
     }
-
-    pub fn push<P: EntryPlugin + 'static>(&mut self, plugin: P) {
+    pub fn push_plugin<P: EntryPlugin + 'static>(&mut self, plugin: P) {
         self.plugins.push(Box::new(plugin));
     }
-    pub fn start(&mut self, config: &Config) {
+    pub fn start(&mut self) {
         for plugin in &mut self.plugins {
-            plugin.start(&config);
+            plugin.start(&self.config);
         }
+        while let Some(()) = self.load_next_entry() {}
     }
-}
-
-impl Iterator for PluginsGroup {
-    type Item = ListEntry;
-    fn next(&mut self) -> Option<Self::Item> {
-        for plugin in &mut self.plugins {
-            if let Some(res) = plugin.next() {
-                return Some(res);
+    pub fn search_loaded(&mut self, key: &str) -> Vec<ListEntry> {
+        let mut retvl = Vec::new();
+        let mut retlen = 0;
+        for ent in self.entries.values() {
+            if matches_search(key, ent) {
+                retlen += ent.expanded_length(1);
+                retvl.push(ent.clone());
+            } else {
+                for child in ent
+                    .children
+                    .iter()
+                    .filter(|child| matches_search(key, child))
+                {
+                    retvl.push(child.clone());
+                    retlen += 1;
+                }
+            }
+            if retlen >= self.config.list_size {
+                break;
             }
         }
-        None
+
+        retvl
+    }
+
+    pub fn all_entries(&self) -> Vec<ListEntry> {
+        self.entries.values().cloned().collect()
+    }
+
+    fn load_next_entry(&mut self) -> Option<()> {
+        let ent = self.plugins.iter_mut().find_map(|plugin| plugin.next());
+        let mut ent = match ent {
+            Some(n) => n,
+            None => {
+                return None;
+            }
+        };
+        if let Some(cur_ent) = self.entries.get_mut(ent.exec_name().unwrap()) {
+            let should_replace = (ent.exec_command.len() == 1 && cur_ent.exec_command.len() != 1)
+                || (ent.display_name.is_some() && cur_ent.display_name.is_none());
+            if should_replace {
+                for child in cur_ent.children.drain(..) {
+                    ent.children.push(child);
+                }
+                std::mem::swap(cur_ent, &mut ent);
+            }
+            let should_push = ent.exec_command != cur_ent.exec_command
+                && (ent.exec_command.len() != 1 || cur_ent.exec_command.len() != 1);
+            if should_push {
+                cur_ent.children.push(ent);
+            }
+        } else {
+            self.entries
+                .insert(ent.exec_name().unwrap().to_owned(), ent);
+        }
+        Some(())
     }
 }
